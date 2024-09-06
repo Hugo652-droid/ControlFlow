ea4e1aa174c51d944b01fa0e75f4b06651aafa40
pb72yvp <143070157+Hugo652-droid@users.noreply.github.com>
2024-09-06 14:10:28 +0200

    Labo_1

---
+++ src/weeklyDayLoop.js
@@ -0,0 +1,12 @@
+function getDayBoucle(day){
+    let i = 0
+    let tab_Jour = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche']
+    for (i = 1; i < 8; i++){
+        if (i === day){
+            return tab_Jour[day-1]
+        }
+    }
+    return "Le chiffre n'est pas valide"
+}
+
+console.log(getDayBoucle(4))
